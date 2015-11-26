db2208375766016897994c5337c90ecce446fd1e
D. Vespa <d.vespa@gmail.com>
2015-11-26 17:20:39 -0200

    criando esqueleto do projeto

---
+++ Gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    del = require("del");
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+//dev
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: ['./app/'],
+    }
+  });
+  gulp.watch('dev/styles/**/*.scss', ["styles",reload]);
+ // gulp.watch('dev/javascript/**/*.js');
+ // gulp.watch('dev/images/**/*.js');
+});
+
+
+gulp.task('styles', function () {
+  return gulp.src([
+      './dev/style/**.scss'
+    ])
+    .pipe(sass())
+    .pipe(gulp.dest('./app/style/'))
+});
+//
+gulp.task('clean', function(cb) {
+  del([ "./app"],cb);
+});
+
+gulp.task("build", ["clean"], function(cb){
+
+
+});
+
+
+gulp.task('default', ['build'], function () {
+    gulp.run('browser-sync');
+});
+
