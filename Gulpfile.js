0b73edbb7ce3523d3f1be8a78bbe4ed6858f47b8
D. Vespa <d.vespa@gmail.com>
2015-11-29 17:36:30 -0200

    Gulp 'ouvindo' pasta de imagens

--- Gulpfile.js
+++ Gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('browser-sync', function() {
   gulp.watch('./dev/style/**/*.scss', ["styles",reload]);
   gulp.watch('./dev/**/*.html', ["move",reload]);
   //gulp.watch('dev/javascript/**/*.js');
- // gulp.watch('dev/images/**/*.js');
+  gulp.watch('dev/images/**/*',["move",reload]);
 });
 
 
@@ -41,7 +41,7 @@ gulp.task('styles', function () {
       './dev/style/main.scss'
     ])
     .pipe(sass())
-  //  .pipe(minifyCss())
+    .pipe(minifyCss())
     .pipe(gulp.dest('./app/style/'))
 });
 //
